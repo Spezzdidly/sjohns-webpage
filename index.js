229fd24c7ba60b89e31856e1bfd054d0f379ba3d
Spezzdidly <s.johns3404@gmail.com>
2026-06-30 15:55:26 -0400

    Serve js and css assets and render the homepage header with JavaScript.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/js", express.static(path.join(__dirname, "js")));
+app.use("/css", express.static(path.join(__dirname, "css")));
 
 app.listen(8080, () => {
 	console.log("The server is running");
---
+++ js/header.js
@@ -0,0 +1,3 @@
+const header = document.querySelector("#siteHeader");
+
+header.innerHTML = '<h1>Welcome to SpencerJohns.com</h1>';
